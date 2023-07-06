76f468639a8389ac325efa374e267b742cc89fb2
surajn17 <131229386+surajn17@users.noreply.github.com>
2023-07-07 01:09:01 +0530

    Create wordpress.sh
    deploy of wordpress

---
+++ wordpress.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Check if Docker is installed
+if ! command_exists docker; then
+    echo "Docker is not installed. Installing Docker..."
+    # Install Docker
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker "$USER"
+    rm get-docker.sh
+    echo "Docker has been installed."
+else
+    echo "Docker is already installed."
+fi
+
+# Check if Docker Compose is installed
+if ! command_exists docker-compose; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+    # Install Docker Compose
+    sudo curl -fsSL -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo "Docker Compose has been installed."
+else
+    echo "Docker Compose is already installed."
+fi
+
+# Create a directory for the site
+mkdir "$site_name"
+cd "$site_name" || exit
+
+# Create docker-compose.yml file
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  db:
+    image: mysql:5.7
+    volumes:
+      - db_data:/var/lib/mysql
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: example
+      MYSQL_DATABASE: wordpress
+      MYSQL_USER: wordpress
+      MYSQL_PASSWORD: wordpress
+
+  wordpress:
+    depends_on:
+      - db
+    image: wordpress:latest
+    volumes:
+      - ./wp-content:/var/www/html/wp-content
+    restart: always
+    ports:
+      - "80:80"
+    environment:
+      WORDPRESS_DB_HOST: db:3306
+      WORDPRESS_DB_USER: wordpress
+      WORDPRESS_DB_PASSWORD: wordpress
+      WORDPRESS_DB_NAME: wordpress
+
+volumes:
+  db_data:
+EOF
+
+# Start the containers
+docker-compose up -d
+
+echo "WordPress site has been created. You can access it at http://localhost." 
